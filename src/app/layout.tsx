5dc493abc28fa674b81cc496361b44df85d4966c
abzalt1 <abzal46@gmail.com>
2026-07-12 10:28:24 +0500

    chore: add Yandex Metrica tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,23 @@ export default function RootLayout({
  fbq('track', 'PageView');
  `}
  </Script>
+        <Script id="yandex-metrica" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=110617911', 'ym');
+
+            ym(110617911, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+ <noscript>
+   <div>
+     <img src="https://mc.yandex.ru/watch/110617911" style={{ position: "absolute", left: "-9999px" }} alt="" />
+   </div>
+ </noscript>
  {children}
  </body>
  </html>
